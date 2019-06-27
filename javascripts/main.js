5cb1e79a85cd2c1e716488b8275babf3400230ed
dax <info@herbalalchemy.eu>
2019-06-27 19:49:31 +0200

    WIP Work on updating site

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,76 +6,43 @@ $(document).ready(function() {
     console.log("window width: "+$(window).width()+"px");
   };
 
+  // nav switching
   $(".tab-button").on("click", function(){
     var selected = $(this).attr('id').replace('-button', '');
 
+    // convert to secondary page style on initial nav click
+    // hide initally visible large logo
+    $("#header-wrap").hide();
+    // convert nav styling
+    $("#nav-wrap").addClass('top-nav-bar');
+
     // clear 'selected' and designate the currently selected tab-button
     $("#nav>li.selected").removeClass("selected");
+    $(".sub-nav>li.selected").removeClass("selected");
     $(this).addClass("selected");
 
     // hide all tabs and show the selected tab
     $(".tab").hide();
     $("#"+selected).show();
 
-    // change the background color to match the selected element
-    changeBackgroundColor(selected, defaultBackgroundColor);
-    rotateSeal(selected);
+    // show sub-navigation if relevant
+    if($("#"+selected).hasClass("sub-navigable")) {
+      $("#"+selected+"-sub-nav").show(); // show the sub-nav of element
+      // automatically select the first sub-nav
+      var firstSubNav = $("#"+selected+"-sub-nav").children().first();
+      firstSubNav.addClass("selected");
+      // automatically display the first sub-tab
+      var firstSubTab = $("#"+selected).next();
+      firstSubTab.show();
+    } else if($("#"+selected).hasClass("sub-tab")) {
+      // re-select the parent nav button to clarify sub-nav origin
+      var parentNavButton = this.parentNode.id.replace('sub-nav', 'button');
+      $("#"+parentNavButton).addClass("selected");
+    } else {
+      $(".sub-nav").hide();
+    };
   });
 });
-
-function changeBackgroundColor(selected, defaultBackgroundColor) {
-   var color = colorForPlanet(selected, defaultBackgroundColor);
-   $('html').css({
-     "background-color": color,
-     "-webkit-transition": "background 2s ease",
-     "-moz-transition": "background 2s ease",
-     "-ms-transition": "background 2s ease",
-     "-o-transition": "background 2s ease",
-     "transition": "background 2s ease"
-   });
-}
-
-function rotateSeal(selected) {
-  var updatedLogoRotation = logoRotation(selected);
-  $('#logo').css({
-    'transform': 'rotate('+updatedLogoRotation+'deg)',
-     "-webkit-transition": "all 2s ease",
-     "-moz-transition": "all 2s ease",
-     "-ms-transition": "all 2s ease",
-     "-o-transition": "all 2s ease",
-     "transition": "all 2s ease"
-  });
-}
-
-function colorForPlanet(selected, defaultBackgroundColor) {
-  switch(selected){
-    case 'mercury':
-      return '#8a7e57';
-      break;
-    case 'venus':
-      return '#548a63';
-      break;
-    case 'luna':
-      return '#666';
-      break;
-    default: return defaultBackgroundColor;
-  }
-}
-
-function logoRotation(selected) {
-  switch(selected){
-    case 'mercury':
-      return '103'
-      break;
-    case 'venus':
-      return '0';
-      break;
-    case 'luna':
-      return '51';
-      break;
-    default: return '154';
-  }
-}
 
 goToPage = function(path){
   // update urls when clicking a link

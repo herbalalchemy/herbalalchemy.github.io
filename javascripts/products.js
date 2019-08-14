9df6252f1f6fccf765b44584d88834fd184525c3
dax <info@herbalalchemy.eu>
2019-08-14 23:09:14 +0200

    Product tab updates

--- javascripts/products.js
+++ javascripts/products.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   //product-detail switching
-  $("#smoking-blend-symbols-nav li").on("click", function(){
+  $("#product-symbols-nav li").on("click", function(){
     var selected = this.classList[0]
 
-    $("#smoking-blend-symbols-nav > li.selected").removeClass("selected");
+    $("#product-symbols-nav > li.selected").removeClass("selected");
     $(this).addClass("selected");
 
     $("#product-detail-wrap").children().addClass('hidden');
